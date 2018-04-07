7bf8c4a39322ca1ad4666386917180fc706c2845
William McMurray <tiwilinlove@hotmail.com>
2018-04-07 14:36:13 -0400

    Simpler components importing

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -4,15 +4,10 @@ import Router from 'ROOT/router.js';
 import Store from 'ROOT/store.js';
 
 // components
-import CheckComponent from 'ROOT/views/components/Check.vue'
-import LoaderComponent from 'ROOT/views/components/Loader.vue'
-import ProgressBarComponent from 'ROOT/views/components/ProgressBar.vue'
-import ThreeDotsComponent from 'ROOT/views/components/ThreeDots.vue'
-
-Vue.component('check', CheckComponent);
-Vue.component('loader', LoaderComponent);
-Vue.component('progress-bar', ProgressBarComponent);
-Vue.component('three-dots', ThreeDotsComponent);
+Vue.component('check', require('ROOT/views/components/Check.vue'));
+Vue.component('loader', require('ROOT/views/components/Loader.vue'));
+Vue.component('progress-bar', require('ROOT/views/components/ProgressBar.vue'));
+Vue.component('three-dots', require('ROOT/views/components/ThreeDots.vue'));
 
 var app = new Vue({
   el: '#app',
