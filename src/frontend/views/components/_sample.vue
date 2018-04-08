4ab2c8f9269b12279c0a95321cf9d64ff623d633
William McMurray <tiwilinlove@hotmail.com>
2018-04-08 11:44:10 -0400

    Add sample component to speed up new components creation

---
+++ src/frontend/views/components/_sample.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="sample">
+
+  </div>
+</template>
+
+<script>
+/**
+ * Displays ...
+ *
+ * How to use :
+ * <sample />
+*/
+export default {
+  name: 'sample',
+  data: function(){
+    return {}
+  }
+}
+</script>
+
+<style lang="scss">
+@import "~ROOT/styles/config";
+
+.sample {
+}
+</style>
