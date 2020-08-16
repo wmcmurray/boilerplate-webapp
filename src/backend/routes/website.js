28ced7eeace68cfd3ff35ed0248bf98b81f5f603
wmcmurray <tiwilinlove@hotmail.com>
2020-08-15 21:41:08 -0400

    What, I didn't have a robots.txt

--- src/backend/routes/website.js
+++ src/backend/routes/website.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const websiteController = require('controllers/WebsiteController.js');
 const websiteVariablesMiddleware = require('middlewares/WebsiteVariables.js');
 
-router.get(/.*\.(map|css)$/, websiteController.staticFilesNotFound);
+router.get(/.*\.(map|css|txt)$/, websiteController.staticFilesNotFound);
 router.use(websiteVariablesMiddleware);
 router.get('*', websiteController.entryPoint);
 
