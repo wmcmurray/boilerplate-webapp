583d8eb9bd8733e042a1b0a124502b33f62d9d85
William McMurray <tiwilinlove@hotmail.com>
2021-07-01 18:48:52 -0400

    Implement the HeroPicture component

---
+++ src/frontend/common/views/components/HeroPicture.vue
@@ -0,0 +1,131 @@
+<template>
+  <div :class="'hero-picture bg-inverted content-align-'+alignContent" :style="'background-image: url('+backgroundImage+');'">
+    <div class="shadow-gradient" />
+    <div class="inner-limit">
+      <div class="inner-content">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+/**
+ * Displays content inside a box that emphasise a large picture in the background
+ *
+ * How to use :
+ * <hero-picture backgroundImage="STRING" alignContent="left" />
+*/
+export default {
+  name: 'hero-picture',
+  props: {
+
+    // url of the background image
+    backgroundImage: {
+      type: String,
+      default: 'https://placeimg.com/1000/480/arch',
+    },
+
+    // where content is aligned ( left | center | right)
+    alignContent: {
+      type: String,
+      default: 'left',
+    },
+
+    // if a shadow gradient is added (to add contrast to the text)
+    shadowGradient: {
+      type: Boolean,
+      default: true,
+    },
+
+  },
+  data(){
+    return {};
+  },
+}
+</script>
+
+<style lang="scss">
+@import "~ROOT/styles/config.scss";
+
+.hero-picture {
+  $shadowGradientColor: black;
+  $shadowGradientOpacity: 0.6;
+
+  position: relative;
+  padding: $globalSpacing;
+  box-sizing: border-box;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+
+  @media (max-width: $xs - 1px){
+    padding: ($globalPadding * 2) $globalPadding;
+  }
+
+  > .shadow-gradient {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    width: 100%;
+    height: 100%;
+  }
+
+  > .inner-limit {
+    position: relative;
+    display: flex;
+    align-items: center;
+    min-height: 75vh;
+    max-width: $maxWidth * 1.25;
+    margin-left: auto;
+    margin-right: auto;
+
+    @media (max-width: $xs - 1px){
+      min-height: initial;
+    }
+
+    > .inner-content {
+      max-width: $maxWidth * 0.5;
+      font-size: 1.25em;
+      line-height: $globalLineHeight;
+      text-shadow: 0px 1px 0px rgba(black, 0.2);
+    }
+  }
+
+  &.content-align-left {
+    background-position: right center;
+
+    > .shadow-gradient {
+      background-image: linear-gradient(75deg, rgba($shadowGradientColor, $shadowGradientOpacity) 50%, rgba($shadowGradientColor, 0));
+    }
+
+    > .inner-limit {
+      justify-content: flex-start;
+    }
+  }
+
+  &.content-align-center {
+    background-position: center;
+
+    > .shadow-gradient {
+      background-image: linear-gradient(90deg, rgba($shadowGradientColor, 0), rgba($shadowGradientColor, $shadowGradientOpacity) 30% 70%, rgba($shadowGradientColor, 0));
+    }
+
+    > .inner-limit {
+      justify-content: center;
+    }
+  }
+
+  &.content-align-right {
+    background-position: left center;
+
+    > .shadow-gradient {
+      background-image: linear-gradient(-75deg, rgba($shadowGradientColor, $shadowGradientOpacity) 50%, rgba($shadowGradientColor, 0));
+    }
+
+    > .inner-limit {
+      justify-content: flex-end;
+    }
+  }
+}
+</style>
--- src/frontend/views/pages/Components.vue
+++ src/frontend/views/pages/Components.vue
@@ -40,6 +40,13 @@
 
     <hr>
 
+    <hero-picture>
+      <h2>Hero picture title</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <button type="button" class="inverted">click here</button>
+    </hero-picture>
+
     <section id="tabs-section" class="limit-width padded">
       <tabs>
 
@@ -192,10 +199,14 @@
 
 <script>
 import AbstractPage from 'COMMON/views/pages/AbstractPage.vue'
+import HeroPicture from 'COMMON/views/components/HeroPicture.vue'
 
 export default {
   name: 'components-page',
   extends: AbstractPage,
+  components: {
+    HeroPicture,
+  },
   data(){
     return {
       currProgress: 25,
